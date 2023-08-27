4dba8cd7be5a85b6498fc18822cad41db0ad2a14
PonDad <wsf4666@gmail.com>
2023-08-28 07:39:41 +0900

    add files

---
+++ chatper3/ex_bot_listener.py
@@ -0,0 +1,87 @@
+'''
+ex_bot_listener.py
+
+ウェイクワードを認識してサウンドを鳴らし、コマンド入力を待機する音声認識プログラムです。
+コマンドが入力されると適切なコマンド名を返し、終了コマンドが入力されると音声入力を終了します。
+'''
+
+import json
+from pathlib import Path
+
+from vosk import Model, KaldiRecognizer
+import pyaudio
+
+# Jsonファイルからウェイクワードとコマンドの配列を読み込む
+with open(Path("data/command_data.json"), "rb") as f:
+    data = json.load(f)
+
+WAKE = data["wake"]
+EXIT = data["exit"]
+
+# Voskモデルの読み込み
+model = Model(str(Path("vosk-model-small-ja-0.22").resolve()))
+
+# マイクの初期化
+recognizer = KaldiRecognizer(model, 16000)
+mic = pyaudio.PyAudio()
+
+# voskの初期化
+def engine():
+    stream = mic.open(format=pyaudio.paInt16,
+                       channels=1, 
+                       rate=16000, 
+                       input=True, 
+                       frames_per_buffer=8192)
+    
+    while True:
+        stream.start_stream()
+        try:
+            data = stream.read(4096)
+            if recognizer.AcceptWaveform(data):
+                result = recognizer.Result()
+                response_json = json.loads(result)
+                print("🖥️ SYSTEM: ", response_json)
+                response = response_json["text"].replace(" ","")
+                return response
+            else:
+                pass
+        except OSError:
+            pass
+
+# ウェイクワード待機をlistening コマンド待機をhearingと設定
+listening = True
+hearing = False
+
+# listeningをループして音声認識 ウェイクワード認識でhearingループする
+def bot_listen_hear():
+    global listening, hearing
+    
+    if hearing == True: print("🖥️ SYSTEM: ","-"*22, "GPTに話しかけてください","-"*22)
+    else: print("🖥️ SYSTEM: ","-"*22, "ウェイクワード待機中","-"*22)
+    
+    while listening:
+        response = engine()
+        if response in WAKE:
+            listening = False
+            hearing = True
+            print("🖥️ SYSTEM: ","-"*22, "GPTに話しかけてください","-"*22)
+        elif response.strip() == "":
+            continue  # 空白の場合はループを続ける
+        else:
+            pass
+    
+    while hearing:
+        response = engine()
+        if response in EXIT:
+            listening = True
+            hearing = False
+        elif response.strip() == "":
+            continue  # 空白の場合はループを続ける
+        else:
+            pass
+        return response 
+
+if __name__ == "__main__":
+    while True:
+        response = bot_listen_hear()
+        print("response: ",response)
---
+++ chatper3/ex_bot_voice_synthesizer.py
@@ -0,0 +1,36 @@
+'''
+ex_bot_voice_synthesizer.py
+
+aquestalkpiを使用して合成音声を出力する音声合成プログラムです。
+「霊夢」と「魔理沙」両方の音声を利用できるようにしています。
+speak()関数とspeak_popen()関数により、同期、非同期の音声再生を提供します。
+'''
+
+import subprocess
+from pathlib import Path
+
+# 音声合成して再生（再生が完了するまで待機）
+def speak(text, num):
+    AquesTalkPi = str(Path("aquestalkpi/AquesTalkPi").resolve())
+    speak_cmd = "echo " + text + " | " + AquesTalkPi + " -b -v f" + str(num) + " -f - | aplay"
+    subprocess.run(speak_cmd,shell=True)
+
+# 音声合成して再生（非同期再生）
+def speak_popen(text, num):
+    AquesTalkPi = str(Path("aquestalkpi/AquesTalkPi").resolve())
+    speak_p_cmd = "echo " + text + " | " + AquesTalkPi + " -b -v f" + str(num) + " -f - | aplay"
+    subprocess.Popen(speak_p_cmd,shell=True)
+
+# 通知音を再生
+def notification():
+    notification_wav = str(Path("data/notificationx4.wav").resolve())
+    print(notification_wav)
+    aplay_cmd = "aplay " + notification_wav
+    subprocess.run(aplay_cmd,shell=True)
+    
+if __name__ == "__main__":
+    notification()
+    speak("テストです", 1)
+    speak("テストです", 2)
+    speak_popen("テストです", 1)
+    speak("テストです", 2)
