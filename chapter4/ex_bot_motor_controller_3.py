ad1eae09fa11ca68762d092cfc83fb5600656404
PonDad <wsf4666@gmail.com>
2023-09-01 15:09:00 +0900

    Update ex_bot_motor_controller_3.py

--- chapter4/ex_bot_motor_controller_3.py
+++ chapter4/ex_bot_motor_controller_3.py
@@ -1,16 +1,16 @@
-import time # ---（※1）
+import time # ---(※1)
 import pantilthat
 
-pantilthat.light_mode(pantilthat.WS2812) # NeoPixelsStick8はWS2812 ---（※2）
-pantilthat.light_type( 1 ) # RGBで設定初期化 ---（※3）
+pantilthat.light_mode(pantilthat.WS2812) # NeoPixelsStick8はWS2812 ---(※2)
+pantilthat.light_type( 1 ) # RGBで設定初期化 ---(※3)
 
-# 目の設定 ネオピクセルLEDの制御 2つのLED指定 ---（※4）
+# 目の設定 ネオピクセルLEDの制御 2つのLED指定 ---(※4)
 def neopixels_face():
     pantilthat.set_pixel(1, 50, 50, 50)
     pantilthat.set_pixel(6, 50, 50, 50)
     pantilthat.show()
 
-# ネオピクセルLEDの消灯 ---（※5）
+# ネオピクセルLEDの消灯 ---(※5)
 def neopixels_off():
     pantilthat.clear()
     pantilthat.show()
