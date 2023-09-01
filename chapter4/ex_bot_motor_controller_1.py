9a8bd9cb3d4e04d4c855b5378a1537c19e6abe99
PonDad <wsf4666@gmail.com>
2023-09-01 14:47:18 +0900

    Update ex_bot_motor_controller_1.py

--- chapter4/ex_bot_motor_controller_1.py
+++ chapter4/ex_bot_motor_controller_1.py
@@ -1,7 +1,7 @@
-import time # ---（※1）
+import time # ---(※1)
 import pantilthat
 
-# パンチルトモーター制御 ｰpan tilt とも -90 ~ 90 ---（※2）
+# パンチルトモーター制御 ｰpan tilt とも -90 ~ 90 ---(※2)
 def pan_tilt(pan, tilt):
     pantilthat.pan(pan)
     pantilthat.tilt(tilt)
@@ -10,23 +10,23 @@ def pan_tilt(pan, tilt):
 # パンチルトモーターをゆっくり動かす speed=10 前後で調整
 def pan_tilt_slow(pan, tilt, speed):
 
-    start_pan = pantilthat.get_pan() # ---（※3）
+    start_pan = pantilthat.get_pan() # ---(※3)
     start_tilt = pantilthat.get_tilt()
     print("start_pan: ",start_pan, " start_tilt: ",start_tilt)
 
-    move_pan = (pan - start_pan) / 100 # ---（※4）
+    move_pan = (pan - start_pan) / 100 # ---(※4)
     move_tilt = (tilt - start_tilt)  / 100
     print("move_pan: ", move_pan, " move_tilt: ", move_tilt)
 
-    cnt = 0 # ---（※5）
+    cnt = 0 # ---(※5)
     while True:
-        pan_tilt(start_pan + move_pan*cnt, start_tilt + move_tilt*cnt) # ---（※6）
+        pan_tilt(start_pan + move_pan*cnt, start_tilt + move_tilt*cnt) # ---(※6)
         cnt += 1
-        if cnt == 100: # ---（※7）
+        if cnt == 100: # ---(※7)
             break
         else:
             pass
-        time.sleep(0.001 * speed) # ---（※8）
+        time.sleep(0.001 * speed) # ---(※8)
 
 if __name__ == "__main__":
     pan_tilt(0, -90)
